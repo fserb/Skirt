8cf011f453842194c4591c96d98f375c62a5f45c
Fernando Serboncini <fserb@fserb.com.br>
2019-08-28 01:37:27 -0400

    final book2 scene

--- scene.js
+++ scene.js
@@ -7,7 +7,7 @@ import {Lambertian, Metal, Dielectric,
   DiffuseLight
 } from "./texture.js";
 
-function _setup1(width, height, RNG) {
+function _setup1(width, height, _RNG) {
   const world = new HitList();
 
   world.push(new Sphere(v3.new(0, 0, -1), 0.5,
@@ -151,7 +151,7 @@ function _setup(width, height, RNG) {
   return [world, camera];
 }
 
-export default function _setup4(width, height, RNG) {
+function _setup4(width, height, _RNG) {
   let world = new HitList();
 
   const red = new Lambertian(new ConstantTexture(0.65, 0.05, 0.05));
@@ -182,6 +182,85 @@ export default function _setup4(width, height, RNG) {
 
   world.push(new ConstantMedium(box1, 0.01, new ConstantTexture(1, 1, 1)));
   world.push(new ConstantMedium(box2, 0.01, new ConstantTexture(0, 0, 0)));
+
+  world = new BVH(world);
+
+  const lookfrom = v3.new(278, 278, -800);
+  const lookat = v3.new(278, 278, 0);
+  const distToFocus = 10.0;
+  const aperture = 0.0;
+  const vfov = 40;
+
+  const camera = new Camera(
+    lookfrom, lookat,
+    v3.new(0, 1, 0),
+    vfov, width / height, aperture, distToFocus);
+
+  return [world, camera];
+}
+
+export default function _setup5(width, height, RNG) {
+  let world = new HitList();
+
+  const white = new Lambertian(new ConstantTexture(0.73, 0.73, 0.73));
+  const ground = new Lambertian(new ConstantTexture(0.48, 0.83, 0.53));
+
+  let boxlist = new HitList();
+  for (let i = 0; i < 20; ++i) {
+    for (let j = 0; j < 20; ++j) {
+      const w = 100;
+      const x0 = -1000 + i * w;
+      const z0 = -1000 + j * w;
+      const y0 = 0;
+      const x1 = x0 + w;
+      const y1 = 100 * (RNG() + 0.01);
+      const z1 = z0 + w;
+      boxlist.push(new Box(v3.new(x0, y0, z0), v3.new(x1, y1, z1), ground));
+    }
+  }
+
+  world.push(boxlist);
+
+  const light = new DiffuseLight(new ConstantTexture(7, 7, 7));
+  world.push(new RectXZ(123, 147, 423, 412, 554, light));
+
+  const center = v3.new(400, 400, 200);
+
+  world.push(new Sphere(v3.new(400, 400, 200), 50,
+    new Lambertian(new ConstantTexture(0.7, 0.3, 0.1))));
+  world.push(new Sphere(v3.new(260, 150, 45), 50, new Dielectric(1.5)));
+  world.push(new Sphere(v3.new(0, 150, 145), 50,
+    new Metal(new ConstantTexture(0.8, 0.8, 0.9), 10.0)));
+
+  let boundary = new Sphere(v3.new(360, 150, 145), 70, new Dielectric(1.5));
+  world.push(boundary);
+  world.push(new ConstantMedium(boundary, 0.2, new ConstantTexture(0.2, 0.4, 0.9)));
+
+  boundary = new Sphere(v3.new(0, 0, 0), 5000, new Dielectric(1.5));
+  world.push(boundary);
+  world.push(new ConstantMedium(boundary, 0.0001, new ConstantTexture(1.0, 1.0, 1.0)));
+
+  const earth = new ImageTexture("./textures/2k_earth_daymap.jpg");
+  const loading = [];
+  loading.push(earth.load);
+  const emat = new Lambertian(earth);
+  world.push(new Sphere(v3.new(400, 200, 400), 100, emat));
+
+  const pertext = new NoiseTexture(RNG, 0.1);
+  world.push(new Sphere(v3.new(220, 280, 300), 80, new Lambertian(pertext)));
+
+  const boxlist2 = new HitList();
+  for (let j = 0; j < 1000; ++j) {
+    boxlist2.push(new Sphere(
+      v3.new(165 * (RNG() - 0.5), 165 * (RNG() - 0.5), 165 * (RNG() - 0.5)),
+      10, white));
+  }
+
+  world.push(new Transform(
+    v3.new(-100 + 82.5, 270 + 82.5, 395 + 82.5),
+    v3.new(0, Math.PI / 6, 0),
+    v3.new(1, 1, 1),
+    boxlist2));
 
   world = new BVH(world);
 
