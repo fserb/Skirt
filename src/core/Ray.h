3cbfbcbb4f7123f237f50e404c875c2b2cfcac40
Fernando Serboncini <fserb@fserb.com.br>
2019-09-07 23:37:35 -0400

    Inline ray iostream

--- src/core/Ray.h
+++ src/core/Ray.h
@@ -21,7 +21,7 @@ class Ray {
   float maxT;
 };
 
-std::ostream& operator<<(std::ostream& os, const Ray& r) {
+INLINE std::ostream& operator<<(std::ostream& os, const Ray& r) {
   os << "Ray[o=" << r.origin << ", d=" << r.direction << "]";
   return os;
 }
